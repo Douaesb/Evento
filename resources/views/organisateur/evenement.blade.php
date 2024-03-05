a3e0ffa7b0347a74971d33a4cc87e0ebac71fc9b
Douae sb <douae.sb411@gmail.com>
2024-03-05 17:30:43 +0100

    event mvc

--- resources/views/organisateur/evenement.blade.php
+++ resources/views/organisateur/evenement.blade.php
@@ -28,7 +28,7 @@
                         </button>
                     </div>
                     <!-- Modal body -->
-                    <form class="p-4 md:p-5" action="{{ route('addevenement') }}" method="post">
+                    {{-- <form class="p-4 md:p-5" action="{{ route('addevenement') }}" method="post"> --}}
                         @csrf
                         <div class="grid gap-4 mb-4 grid-cols-2">
                             <div class="col-span-2">
@@ -50,7 +50,7 @@
                             </svg>
                             Add new evenement
                         </button>
-                    </form>
+                    {{-- </form> --}}
                 </div>
             </div>
         </div>
@@ -104,7 +104,7 @@
                                                     d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" />
                                             </svg>
                                         </a>
-                                        <form action="{{ route('deleteevenement', $evenement) }}" method="post">
+                                        {{-- <form action="{{ route('deleteevenement', $evenement) }}" method="post"> --}}
                                             @csrf
                                             @method('DELETE')
                                             <div title="delete">
@@ -118,7 +118,7 @@
                                                 </button>
 
                                             </div>
-                                        </form>
+                                        {{-- </form> --}}
 
                                     </div>
                                 </div>
@@ -152,20 +152,20 @@
                         </div>
                         <!-- Modal body -->
                         <div class="p-4 md:p-5">
-                            <form class="space-y-4"
-                                action="{{ route('updateevenement') }}" method="post">
+                            {{-- <form class="space-y-4"
+                                action="{{ route('updateevenement') }}" method="post"> --}}
                                 @csrf
                                 @method('PUT')
                                 <div class="col-span-2">
-                                    <input type="hidden" name="evenementID" id="editevenementId"
-                                        value="{{ $evenement->id }}">
+                                    {{-- <input type="hidden" name="evenementID" id="editevenementId"
+                                        value="{{ $evenement->id }}"> --}}
                                     <label for=""
                                         class="block mb-2 text-sm font-medium text-gray-900 text-black">evenement
                                         Title</label>
-                                    <input type="text" name="nom" id="editName"
+                                    {{-- <input type="text" name="nom" id="editName"
                                         value="{{ $evenement->nom }}"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-100 border-gray-500 placeholder-gray-400 text-black focus:ring-primary-500 focus:border-primary-500"
-                                        placeholder="Type category name" required="">
+                                        placeholder="Type event name" required=""> --}}
                                 </div>
 
                                 <button type="submit"
@@ -178,14 +178,14 @@
                                     </svg>
                                     Update evenement
                                 </button>
-                            </form>
+                            {{-- </form> --}}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <script>
+    {{-- <script>
         document.addEventListener('DOMContentLoaded', function() {
             const editButtons = document.querySelectorAll('.editevenementButton');
             const editevenementIdInput = document.getElementById('editevenementId');
@@ -207,7 +207,7 @@
                 });
             });
         });
-    </script>
+    </script> --}}
 
 
 </x-app-layout>
