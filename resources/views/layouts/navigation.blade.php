d27b41f15c1032b6af832be7abe758b858e1fd58
Douae sb <douae.sb411@gmail.com>
2024-03-08 12:26:14 +0100

    fix date event

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -167,7 +167,7 @@
                 </li>
                 <li>
                     <a href="{{ route('users') }}"
-                        class="flex flex-row bg-blue-100 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                        class="flex flex-row active items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                         <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                                 class="bx bx-shopping-bag"></i></span>
                         <span class="text-sm font-medium">Users</span>
@@ -175,7 +175,7 @@
                 </li>
                 <li>
                     <a href="{{ route('categories') }}"
-                        class="flex flex-row bg-blue-100 items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                        class="flex flex-row active items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                         <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i
                                 class="bx bx-shopping-bag"></i></span>
                         <span class="text-sm font-medium">Categories</span>
